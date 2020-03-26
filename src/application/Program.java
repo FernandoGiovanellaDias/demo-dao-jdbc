3e93a99fd0e9bf569cbb231e437a7cd217d8cd0f
FernandoGiovanellaDias <fagdias2002@hotmail.com>
2020-03-26 16:21:39 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,12 +20,13 @@ public class Program {
 
 		System.out.println(obj);
 		System.out.println(seller);
+		
 		System.out.println();
-
 		System.out.println("/============== Teste 1_seller findById ========================/");
 		System.out.println();
 
 		System.out.println(seller2);
+		
 		System.out.println();
 		System.out.println("/============== Teste 2_seller findByDepartment ================/");
 		System.out.println();
@@ -35,15 +36,32 @@ public class Program {
 		for (Seller obj1 : list) {
 			System.out.println(obj1);
 		}
+		
+		System.out.println();
+		System.out.println("/============== Teste 3_seller findAll =========================/");
 		System.out.println();
 
+		list = sellerDao.findAll();
+		for (Seller obj1 : list) {
+			System.out.println(obj1);
+		}
+		
+		System.out.println();
 		System.out.println("/============== Teste 4_seller insert ==========================/");
 		System.out.println();
 
 		Seller newSeller = new Seller(null, "Leonardo", "Leonardo@gmail.com", new Date(), 5000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new ID: "+newSeller.getId());
+		
+		System.out.println();
+		System.out.println("/============== Teste 5_seller Update ==========================/");
+		System.out.println();
+
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("UPdate Complected!");
 
 	}
-
 }
